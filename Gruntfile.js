e37dc8d90d1a1454c9d3c84d9376d5d1b03ef734
IjzerenHein <hrutjes@gmail.com>
2014-07-24 08:39:53 +0200

    Added eslint & jscs checking + fixed code-style accordingly

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,71 +4,24 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Task configuration.
-    jshint: {
+    eslint: {
+      target: ['*.js'],
       options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
-        }
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['*.js', 'examples/panToPosition/*.js', 'test/**/*.js']
+        config: '.eslintrc'
       }
     },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
+    jscs: {
+      src: ['*.js'],
+      options: {
+        config: '.jscsrc'
       }
-    },
-    jsdox: {
-      generate: {
-        options: {
-          contentsEnabled: false,
-          contentsTitle: 'Example Documentation',
-          contentsFile: 'readme.md',
-        },
-        src: ['./Lagometer.js'],
-        dest: 'docs'
-      },
-      /*publish: {
-        enabled: true,
-        path: '<%= jsdox.generate.dest %>',
-        message: 'Markdown Auto-Generated for version <%= pkg.version %>',
-        remoteName: 'upstream',
-        remoteBranch: 'master'
-      }*/
     }
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-jsdox');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-jscs-checker');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'jsdox', 'qunit']);
-
+  grunt.registerTask('default', ['eslint', 'jscs']);
 };
--- Lagometer.js
+++ Lagometer.js
@@ -1,16 +1,16 @@
-/* 
+/*
  * Copyright (c) 2014 Gloey Apps
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,7 +18,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- * 
+ *
  * @author: Hein Rutjes (IjzerenHein)
  * @license MIT
  * @copyright Gloey Apps, 2014
@@ -27,14 +27,13 @@
 /*jslint browser:true, nomen:true, vars:true, plusplus:true*/
 /*global define*/
 
-define(function (require, exports, module) {
+define(function(require, exports, module) {
     'use strict';
 
     // import dependencies
     var Engine = require('famous/core/Engine');
     var CanvasSurface = require('famous/surfaces/CanvasSurface');
     var View = require('famous/core/View');
-    
 
     /**
      * @class Lagometer
@@ -44,16 +43,16 @@ define(function (require, exports, module) {
      */
     function Lagometer(options) {
         View.apply(this, arguments);
-        
+
         // Create sample-buffer
         this.samples = [];
         this.sampleIndex = 0;
         this.Samples = this.options.size[0];
-        
+
         // Install render-handlers
         Engine.on('prerender', this._onEngineRender.bind(this, true));
         Engine.on('postrender', this._onEngineRender.bind(this, false));
-        
+
         // Create drawing canvas
         this.canvas = new CanvasSurface();
         this.add(this.canvas);
@@ -72,33 +71,41 @@ define(function (require, exports, module) {
         frameColor: '#00FF00',
         scriptColor: '#BBBBFF'
     };
-    
+
     /**
      * @method _onEngineRender
      */
-    Lagometer.prototype._onEngineRender = function (pre) {
+    Lagometer.prototype._onEngineRender = function(pre) {
         var currentTime = Date.now();
         if (pre) {
-            
+
             // Determine the time that was spent between two 'animation-frames'
             if (this.lastTime !== undefined) {
                 this.frameTime = currentTime - this.lastTime;
-                if (this.maxFrameTime === undefined) { this.maxFrameTime = this.frameTime; }
+                if (this.maxFrameTime === undefined) {
+                    this.maxFrameTime = this.frameTime;
+                }
                 this.maxFrameTime = Math.max(this.frameTime, this.maxFrameTime);
-                if (this.minFrameTime === undefined) { this.minFrameTime = this.frameTime; }
+                if (this.minFrameTime === undefined) {
+                    this.minFrameTime = this.frameTime;
+                }
                 this.minFrameTime = Math.min(this.frameTime, this.minFrameTime);
             }
             this.lastTime = currentTime;
-            
+
         } else if (this.frameTime !== undefined) {
-   
+
             // Determine the time that was spent in the script
             this.scriptTime = currentTime - this.lastTime;
-            if (this.maxScriptTime === undefined) { this.maxScriptTime = this.scriptTime; }
+            if (this.maxScriptTime === undefined) {
+                this.maxScriptTime = this.scriptTime;
+            }
             this.maxScriptTime = Math.max(this.scriptTime, this.maxScriptTime);
-            if (this.minScriptTime === undefined) { this.minScriptTime = this.scriptTime; }
+            if (this.minScriptTime === undefined) {
+                this.minScriptTime = this.scriptTime;
+            }
             this.minScriptTime = Math.min(this.scriptTime, this.minScriptTime);
-            
+
             // Create sample
             var sample = {
                 lastTime: this.lastTime,
@@ -109,20 +116,22 @@ define(function (require, exports, module) {
             if (this.samples.length < maxSamples) {
                 this.sampleIndex = this.samples.length;
                 this.samples.push(sample);
-            } else {
+            }
+            else {
                 this.sampleIndex = (this.sampleIndex + 1) % maxSamples;
                 this.samples[this.sampleIndex] = sample;
             }
         }
     };
-    
+
     /**
      * @method _drawSamples
      */
-    Lagometer.prototype._drawSamples = function (draw) {
-        
+    Lagometer.prototype._drawSamples = function(draw) {
+
         draw.context.beginPath();
-        var i, bufferIndex = draw.index;
+        var i;
+        var bufferIndex = draw.index;
         var size = draw.size;
         var yScale =  size[1] / (draw.max - draw.min);
         for (i = 0; i < draw.buffer.length; i++) {
@@ -131,21 +140,24 @@ define(function (require, exports, module) {
             var y = size[1] - ((sample - draw.min) * yScale);
             if (i === 0) {
                 draw.context.moveTo(x, y);
-            } else {
+            }
+            else {
                 draw.context.lineTo(x, y);
             }
             bufferIndex--;
-            if (bufferIndex < 0) { bufferIndex = draw.buffer.length - 1; }
+            if (bufferIndex < 0) {
+                bufferIndex = draw.buffer.length - 1;
+                }
         }
         draw.context.lineWidth = 1;
         draw.context.strokeStyle = draw.color;
         draw.context.stroke();
     };
-    
+
     /**
      * @method _getFPS
      */
-    Lagometer.prototype._getFPS = function (count) {
+    Lagometer.prototype._getFPS = function(count) {
         count = Math.min(count, this.samples.length);
         var bufferIndex = this.sampleIndex;
         var i;
@@ -154,11 +166,13 @@ define(function (require, exports, module) {
             var sample = this.samples[bufferIndex];
             fps += sample.frameTime;
             bufferIndex--;
-            if (bufferIndex < 0) { bufferIndex = this.samples.length - 1; }
+            if (bufferIndex < 0) {
+                bufferIndex = this.samples.length - 1;
+            }
         }
         return 1000 / (fps / count);
     };
-    
+
     /**
      * Renders the view.
      *
@@ -171,7 +185,7 @@ define(function (require, exports, module) {
         var size = this.getSize();
         var canvasSize = [size[0] * 2, size[1] * 2];
         this.canvas.setSize(size, canvasSize);
-        
+
         // Clear background
         context.clearRect(0, 0, canvasSize[0], canvasSize[1]);
         context.fillStyle = this.options.backgroundColor;
@@ -179,7 +193,7 @@ define(function (require, exports, module) {
         context.lineWidth = 1;
         context.strokeStyle = this.options.borderColor;
         context.strokeRect(0, 0, canvasSize[0], canvasSize[1]);
-        
+
         // Calculate min/max
         var min = this.options.min;
         var max = this.options.max;
@@ -196,15 +210,15 @@ define(function (require, exports, module) {
             }
             min = 0;
         }*/
-        
+
         // Prepare text drawing
         context.fillStyle = this.options.textColor;
         context.font = this.options.font;
-        
+
         // Draw fps (calculated over last 20 frames)
         var fps = Math.round(this._getFPS(20));
         context.fillText(fps + ' fps', canvasSize[0] - 84, 26);
-        
+
         // Draw frame-times
         this._drawSamples({
             context: context,
@@ -216,7 +230,7 @@ define(function (require, exports, module) {
             property: 'frameTime',
             color: this.options.frameColor
         });
-        
+
         // Draw script-times
         this._drawSamples({
             context: context,
@@ -228,10 +242,10 @@ define(function (require, exports, module) {
             property: 'scriptTime',
             color: this.options.scriptColor
         });
-        
+
         // Call super
         return this._node.render();
     };
-    
+
     module.exports = Lagometer;
 });
